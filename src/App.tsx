00d7551857264229bcf776c092a59304db0da15d
Andriikryl <andreyk557@gmail.com>
2023-05-25 19:08:07 +0300

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <div className="navigation"></div>
+      <div className="container">
+        <div className="topBar"></div>
+        <div className="content"></div>
+        <div className="footer"></div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
